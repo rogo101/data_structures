007f3bd9628770c9c464a211d839b88cbf853e39
Raghav Verma <raghavv2@gelib-4c-34.ews.illinois.edu>
2018-09-11 18:44:44 -0500

    Hallefuckinglujah

--- mp1/mp1.cpp
+++ mp1/mp1.cpp
@@ -5,7 +5,7 @@ using cs225::PNG;
 using cs225::HSLAPixel;
 
 #include <string>
-
+#include <iostream>
 
 
 void rotate(std::string inputFile, std::string outputFile) {
@@ -13,13 +13,30 @@ void rotate(std::string inputFile, std::string outputFile) {
   toRotate->readFromFile(inputFile);
   unsigned width = toRotate->width();
   unsigned height = toRotate->height();
+  PNG* rotated = new PNG(width, height);
 
-  
+  for (unsigned y = 0; y < height; y++) {
+      for (unsigned x = 0; x < width; x++) {
+          HSLAPixel& after = (*rotated).getPixel(x, y);
+          after = toRotate->getPixel(width - x - 1, height - y - 1);
+      }
+  }
+
+  rotated->writeToFile(outputFile);
+  delete toRotate;
+  delete rotated;
 }
 
 PNG myArt(unsigned int width, unsigned int height) {
   PNG png(width, height);
-  // TODO: Part 3
+
+  for (unsigned x = 0; x < width; x++) {
+    for (unsigned y = 0; y < height; y++) {
+      HSLAPixel& modify = png.getPixel(x, y);
+      modify.l = 0.5;
+      modify.h = x / 3;
+  }
+}
 
   return png;
 }
