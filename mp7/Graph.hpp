5f6ecfd2b17f13bccc6875168c02f48989331d09
Raghav Verma <raghavv2@linux-07.ews.illinois.edu>
2018-12-12 22:32:09 -0600

    Done

--- mp7/Graph.hpp
+++ mp7/Graph.hpp
@@ -5,13 +5,16 @@
 #include <string>
 #include <iostream>
 
+using namespace std;
+
 /**
 * @return The number of vertices in the Graph
 */
 template <class V, class E>
 unsigned int Graph<V,E>::size() const {
   // TODO: Part 2
-  return 0;
+  unsigned size = vertexMap.size();
+  return size;
 }
 
 
@@ -22,7 +25,9 @@ unsigned int Graph<V,E>::size() const {
 template <class V, class E>
 unsigned int Graph<V,E>::degree(const V & v) const {
   // TODO: Part 2
-  return 0;
+  string keys = v.key();
+  unsigned size = adjList[keys].size();
+  return size;
 }
 
 
@@ -35,6 +40,17 @@ template <class V, class E>
 V & Graph<V,E>::insertVertex(std::string key) {
   // TODO: Part 2
   V & v = *(new V(key));
+  list<edgeListIter> graphIterator;
+  // std::pair<string, V &> hashPair(key, v);
+  // vertexMap.insert(hashPair);
+  pair<string, V &> pair1 (key, v);
+  pair<string, list<edgeListIter>> pair2 (key, graphIterator);
+  adjList.insert(pair2);
+  vertexMap.insert(pair1);
+  // //create new pair and insert into adjacent List
+  // std::list<edgeListIter> it;
+  // std::pair<string, std::list<edgeListIter>> hashPair2(key, it);
+  // adjList.insert(hashPair2);
   return v;
 }
 
@@ -46,6 +62,8 @@ V & Graph<V,E>::insertVertex(std::string key) {
 template <class V, class E>
 void Graph<V,E>::removeVertex(const std::string & key) {
   // TODO: Part 2
+  vertexMap.eraseKey(); //utilizes earseKey function
+  adjList.eraseKey();
 }
 
 
@@ -59,7 +77,23 @@ template <class V, class E>
 E & Graph<V,E>::insertEdge(const V & v1, const V & v2) {
   // TODO: Part 2
   E & e = *(new E(v1, v2));
+  //
+  // string key1 = v1.key();
+  // string key2 = v2.key();
+  // edgeListIter it = edgeList.begin();
+  //
+  edgeList.push_front(e);
+  edgeListIter graphIterator = edgeList.begin();
+  string v1Key = v1.key();
+  string v2Key = v2.key();
 
+  adjList[v1Key].push_front(graphIterator);
+  adjList[v2Key].push_front(graphIterator);
+  //
+  // edgeList.push_front(e);
+  // adjList[key1].push_front(it);
+  // adjList[key2].push_front(it);
+  ///
   return e;
 }
 
@@ -70,8 +104,9 @@ E & Graph<V,E>::insertEdge(const V & v1, const V & v2) {
 * @param key2 The key of the destination Vertex
 */
 template <class V, class E>
-void Graph<V,E>::removeEdge(const std::string key1, const std::string key2) {  
+void Graph<V,E>::removeEdge(const std::string key1, const std::string key2) {
   // TODO: Part 2
+  return;
 }
 
 
@@ -83,6 +118,7 @@ void Graph<V,E>::removeEdge(const std::string key1, const std::string key2) {
 template <class V, class E>
 void Graph<V,E>::removeEdge(const edgeListIter & it) {
   // TODO: Part 2
+  return;
 }
 
 
@@ -90,11 +126,19 @@ void Graph<V,E>::removeEdge(const edgeListIter & it) {
 * @param key The key of an arbitrary Vertex "v"
 * @return The list edges (by reference) that are adjacent to "v"
 */
-template <class V, class E>  
+template <class V, class E>
 const std::list<std::reference_wrapper<E>> Graph<V,E>::incidentEdges(const std::string key) const {
   // TODO: Part 2
-  std::list<std::reference_wrapper<E>> edges;
-  return edges;
+  list<reference_wrapper<E>> returnEdge;
+  for(reference_wrapper<E> curr : edgeList) {
+    if(curr.get().source().key() == key) {
+      returnEdge.push_back(curr);
+    }
+    if (curr.get().dest().key() == key) {
+      returnEdge.push_back(curr);
+    }
+  }
+  return returnEdge;
 }
 
 
@@ -107,5 +151,15 @@ const std::list<std::reference_wrapper<E>> Graph<V,E>::incidentEdges(const std::
 template <class V, class E>
 bool Graph<V,E>::isAdjacent(const std::string key1, const std::string key2) const {
   // TODO: Part 2
+  list<edgeListIter> prev = adjList.at(key1),  curr = adjList.at(key2),  flag;
+
+  flag = (prev.size() > curr.size()) ? curr : prev;
+  for (edgeListIter iterator3 : flag) {
+    if(((Edge&)*iterator3).source().key() == key1) {
+      if (((Edge&)*iterator3).dest().key() == key2) {
+        return true;
+      }
+    }
+  }
   return false;
 }
--- mp7/Graph2.hpp
+++ mp7/Graph2.hpp
@@ -3,23 +3,91 @@
 #include <string>
 #include <list>
 
+using namespace std;
+
 /**
  * Returns an std::list of vertex keys that creates some shortest path between `start` and `end`.
  *
  * This list MUST include the key of the `start` vertex as the first vertex in the list, the key of
  * the `end` vertex as the last element in the list, and an ordered list of all vertices that must
  * be traveled along the shortest path.
- * 
+ *
  * For example, the path a -> c -> e returns a list with three elements: "a", "c", "e".
- * 
+ *
  * @param start The key for the starting vertex.
  * @param end   The key for the ending vertex.
  */
+
 template <class V, class E>
 std::list<std::string> Graph<V,E>::shortestPath(const std::string start, const std::string end) {
-  std::list<std::string> path;
+  list<string> path;
+  queue<string> q;
+  bool directed;
+  directed = edgeList.front().get().directed();
 
+  for(auto it : edgeList) {
+    it.get()["label"] = "unexplored";
+  }
+  for(auto it : vertexMap) {
+    it.second["label"] = "unexplored";
+  }
+  vertexMap.at(start)["p"] = "";
+  vertexMap.at(start)["label"] = "explored";
+  q.push(start);
 
-  return path;
+  while(q.empty() == false)
+  {
+    string current= q.front();
+    q.pop();
+    list<reference_wrapper<E>> edges = incidentEdges(current);
+    for(auto it : edges)
+    {
+      if(it.get()["label"] == "unexplored")
+      {
+        Vertex & dest = vertexMap.at(it.get().dest().key());
+        Vertex & source = vertexMap.at(it.get().source().key());
+        if(!directed){
+          if(source["label"] == "unexplored"){
+              source["label"] = "visited";
+              source["p"] = dest.key();
+              q.push(source.key());
+              it.get()["label"] = "discovery";
+            }
+           if(dest["label"] == "unexplored"){
+            dest["label"] = "visited";
+            dest["p"] = source.key();
+            q.push(dest.key());
+            it.get()["label"] = "discovery";
+          }
+          if(dest["label"] == "visited"){
+            if(source["label"] == "visited"){
+              it.get()["label"] = "cross";
+            }
+          }
+        }
+        if(directed) {
+          if(dest["label"] == "visited"){
+            it.get()["label"] = "cross";
+          }
+          if(dest["label"] == "unexplored"){
+          dest["label"] = "visited";
+          dest["p"] = source.key();
+          q.push(dest.key());
+          it.get()["label"] = "discovery";
+        }
+      }
+    }
+  }
 }
 
+  string curr = end;
+  Vertex v = vertexMap.at(curr);
+  path.push_front(curr);
+
+  while(vertexMap.at(curr)["p"] != "") {
+    curr = v["p"];
+    v = vertexMap.at(curr);
+    path.push_front(curr);
+  }
+  return path;
+}
